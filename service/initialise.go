6f20df34be3ac19e62a1b02a82496643724f72b6
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2025-04-29 10:04:35 +0100

    Upgrade deps

--- service/initialise.go
+++ service/initialise.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/ONSdigital/dp-healthcheck/healthcheck"
 	"github.com/ONSdigital/dp-legacy-cache-proxy/config"
-	dphttp "github.com/ONSdigital/dp-net/v2/http"
+	dphttp "github.com/ONSdigital/dp-net/v3/http"
 )
 
 // ExternalServiceList holds the initialiser and initialisation state of external services.
